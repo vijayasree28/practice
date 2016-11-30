830a72c812e43db3e33a3ebe75253582237a3817
Kolukuluri Vijayasree <vijayasreekolukuluri@gmail.com>
2016-11-30 12:38:47 +0530

    first commit

---
+++ Eveningsession/src/progs/ObjectWrite.java
@@ -0,0 +1,12 @@
+package progs;
+
+import java.io.FileOutputStream;
+
+public class ObjectWrite {
+ public static void main(String[] args) {
+	//try
+	{
+		//FileOutputStream fos= new FileOutputStream("d:") 
+	}
+}
+}
